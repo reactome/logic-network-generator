78ba7c431d7b6fdf8a7ef2215d83e8f29e878fa9
tbarbazuk <156703245+tbarbazuk@users.noreply.github.com>
2024-01-19 16:24:27 -0500

    Update create-denormalized-pathways.py

--- create-denormalized-pathways.py
+++ create-denormalized-pathways.py
@@ -23,8 +23,10 @@ def parse_args():
     parser = argparse.ArgumentParser(description='pathway_creation')
     parser.add_argument('--debug', action='store_true', help='Enable debugging')
     parser.add_argument('--verbose', action='store_true', help='Enable verbose logging')
-    parser.add_argument('--output', type=str, default='pathways_list.tsv', help='Output TSV file for pathways list')
     parser.add_argument('--decompose', action='store_true', help='Decompose sets')
+    parser.add_argument('--input_file', type=str, help='Input file containing pathway information')
+
+	
 
     return parser.parse_args()
 
@@ -322,36 +324,33 @@ def generate_pathway_file(pathway_id, taxon_id, pathway_name, decompose=False):
     pathway_pi_df = create_pathway_pi_df(reaction_inputs_and_outputs_df, reaction_connections_df)
     exit()
 
-
 def main():
-    # parse command line arguements
+    # parse command line arguments
     args = parse_args()
 
     # configure logging based on debug flag
     configure_logging(args.debug, args.verbose)
 
     taxon_id = "9606"
-    # pathways = get_all_pathways()
-    pathways = {"69620": "Cell_Cycle_Checkpoints",
-                "5693567": "HDR_through_Homologous_Recombination_HRR_or_Single_Strand_Annealing_SSA_",
-                "453274": "Mitotic_G2-G2_M_phases",
-                "68875": "Mitotic_Prophase",
-                "453279": "Mitotic_G1-G1_S_phases",
-                "1257604": "PIP3_activates_AKT_signaling",
-                "5673001": "RAF_MAP_kinase_cascade",
-                "1227986": "Signaling_by_ERBB2",
-                "195721": "Signaling_by_WNT",
-                "69242": "S_Phase",
-                "3700989": "Transcriptional_Regulation_by_TP53"
-                }
+
+    if args.input_file:
+        # Read pathways from the input file
+        try:
+            pathways_df = pd.read_csv(args.input_file, sep='\t')
+            pathways = dict(zip(pathways_df['ID'], pathways_df['PathwayName']))
+        except Exception as e:
+            logger.error(f"Error reading input file: {e}")
+            return
+    else:
+        logger.error("Input file (--input_file) is required.")
+        return
+
     # create a .tsv file for pathways list
     pathways_list_df = pd.DataFrame(list(pathways.items()), columns=['ID', 'PathwayName'])
     pathways_list_df.to_csv(args.output, sep='\t', index=False)
 
-
     for pathway_id, pathway_name in pathways.items():
         generate_pathway_file(pathway_id, taxon_id, pathway_name, decompose=args.decompose)
-
 
 if __name__ == "__main__":
     main()
