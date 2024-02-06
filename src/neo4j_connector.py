f1e5a81a6f14c6c865f0b58078c4f3995b968693
Adam Wright <adam.wright@wormbase.org>
2024-02-06 15:22:35 -0500

    getting it to work as far as spliting reactions by sets for inputs and outputs.

--- src/neo4j_connector.py
+++ src/neo4j_connector.py
@@ -50,6 +50,10 @@ def get_labels(entity_id):
     query = query_get_labels_template % entity_id
 
     try:
+
+        data =  graph.run(query).data()
+        print(entity_id)
+        print (data[0])
         return graph.run(query).data()[0]["labels"]
     except Exception:
         logger.error("Error in get_labels", exc_info=True)
