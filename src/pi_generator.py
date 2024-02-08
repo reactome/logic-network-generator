998627fed2ba5ba17416b1c023df440f7bb8f00d
Adam Wright <adam.wright@wormbase.org>
2024-02-07 21:44:59 -0500

    started into creating pi files. Looks setup correctly

--- src/pi_generator.py
+++ src/pi_generator.py
@@ -1,57 +1,35 @@
 import pandas as pd
-import pprint
 
 from src.argument_parser import logger
 
-pp = pprint.PrettyPrinter(indent=4)
-
-
-def create_pathway_pi_df(reaction_inputs_and_outputs_df, reaction_connections_df):
-    logger.debug("Adding reaction pairs to pathway_pi_df")
+def create_pathway_pi(decomposed_uid_mapping, reaction_connections, best_matches):
+    logger.debug("Adding reaction pairs to pathway_pi")
 
     columns = {
-        "parent_id": pd.Series(dtype='Int64'),
-        "parent_reaction_id": pd.Series(dtype='Int64'),
-        "parent_decomposed_reaction_id": pd.Series(dtype='str'),
-        "child_id": pd.Series(dtype='Int64'),
-        "child_reaction_id": pd.Series(dtype='Int64'),
-        "child_decomposed_reaction_id": pd.Series(dtype='str'),
-        "common_ids": pd.Series(dtype='str'),  # Common IDs between inputs and outputs
-        "unmatched_inputs": pd.Series(dtype='str'),  # Unmatched input IDs
-        "unmatched_outputs": pd.Series(dtype='str')  # Unmatched output IDs
+        "parent_id": pd.Series(dtype='str'),
+        "child_id": pd.Series(dtype='str'),
+        "pos_neg": pd.Series(dtype='str'),
+        "and_or": pd.Series(dtype='str'),
     }
-    pathway_pi_df = pd.DataFrame(columns)
+    pathway_pi = pd.DataFrame(columns)
 
-    for idx, reaction_connection in reaction_connections_df.iterrows():
+    print(reaction_connections)
+
+    for idx, reaction_connection in reaction_connections.iterrows():
         parent_reaction_id = reaction_connection['parent_reaction_id']
         child_reaction_id = reaction_connection['child_reaction_id']
 
-        parent_inputs = reaction_inputs_and_outputs_df[
-            (reaction_inputs_and_outputs_df['reaction_id'] == parent_reaction_id)
-            & (reaction_inputs_and_outputs_df['input_or_output'] == 'input')
-        ]['decomposed_entity_id'].values
+        print("parent_reaction_id")
+        print(parent_reaction_id)
+        print("child_reaction_id")
+        print(child_reaction_id)
 
-        child_outputs = reaction_inputs_and_outputs_df[
-            (reaction_inputs_and_outputs_df['reaction_id'] == child_reaction_id)
-            & (reaction_inputs_and_outputs_df['input_or_output'] == 'output')
-        ]['decomposed_entity_id'].values
+        # Assuming parent_reaction_id is a variable containing the value to filter by
+        parent_rows = decomposed_uid_mapping[decomposed_uid_mapping['reactome_id'] == parent_reaction_id]
+        child_rows = decomposed_uid_mapping[decomposed_uid_mapping['reactome_id'] == child_reaction_id]
+        print(parent_rows)
+        print(child_rows)
+        print(best_matches)
 
-        common_ids = set(parent_inputs) & set(child_outputs)
-        unmatched_inputs = set(parent_inputs) - common_ids
-        unmatched_outputs = set(child_outputs) - common_ids
 
-        row = {
-            "parent_id": reaction_connection['parent_reaction_id'],
-            "parent_reaction_id": parent_reaction_id,
-            "parent_decomposed_reaction_id": reaction_connection['parent_decomposed_reaction_id'],
-            "child_id": reaction_connection['child_reaction_id'],
-            "child_reaction_id": child_reaction_id,
-            "child_decomposed_reaction_id": reaction_connection['child_decomposed_reaction_id'],
-            "common_ids": '-'.join(map(str, sorted(list(common_ids)))),
-            "unmatched_inputs": '-'.join(map(str, sorted(list(unmatched_inputs)))),
-            "unmatched_outputs": '-'.join(map(str, sorted(list(unmatched_outputs))))
-        }
-
-        pathway_pi_df = pathway_pi_df.append(row, ignore_index=True)
-
-    return pathway_pi_df
+    return pathway_pi
